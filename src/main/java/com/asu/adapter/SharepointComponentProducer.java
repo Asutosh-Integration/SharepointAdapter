324a6c80ab4304528d7e64d9d96978be4355f12e
asmaharana <asmaharana@deloitte.com>
2022-05-26 23:16:28 +0530

    Initial Draft V.0.5

--- src/main/java/com/asu/adapter/SharepointComponentProducer.java
+++ src/main/java/com/asu/adapter/SharepointComponentProducer.java
@@ -19,29 +19,20 @@ package com.asu.adapter;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.camel.Exchange;
+import org.apache.camel.WrappedFile;
 import org.apache.camel.impl.DefaultProducer;
-import org.apache.http.*;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.*;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
+import java.io.*;
 import com.sap.it.api.securestore.SecureStoreService;
 import com.sap.it.api.securestore.UserCredential;
-import com.sap.it.api.securestore.exception.SecureStoreException;
 import com.sap.it.api.ITApiFactory;
 
 /**
@@ -64,15 +55,36 @@ public class SharepointComponentProducer extends DefaultProducer {
 
     public void process(final Exchange exchange) throws Exception {
         
-        String input = exchange.getIn().getBody(String.class);
+        Object input = exchange.getIn().getBody();
+        String type = "";
+        if (input instanceof WrappedFile) {
+            // unwrap file
+            type = "WrappedFile";
+            input = ((WrappedFile) input).getFile();
+        }
+
+        InputStream is;
+        if (input instanceof InputStream) {
+            is = (InputStream) input;
+            type = "InputStream";
+        } else if (input instanceof File) {
+            is = new FileInputStream((File)input);
+            type = "File";
+        } else if (input instanceof byte[]) {
+            is = new ByteArrayInputStream((byte[]) input);
+            type = "Byte";
+        } else {
+            // try as input stream
+            is = exchange.getContext().getTypeConverter().tryConvertTo(InputStream.class, exchange, input);
+            type = "Try";
+        }
         String URL = endpoint.getURL();
         String TenantID = endpoint.getTenantID();
         String Resource = endpoint.getResource();
         String Credential = endpoint.getCredential();
         String FilePath = endpoint.getFilePath();
         String GET_URL;
-        GET_URL = URL;
-        GET_URL = GET_URL + "_api/web/GetFolderByServerRelativePath(DecodedUrl=@a1)/Files/AddUsingPath(DecodedUrl=@a2,AutoCheckoutOnInvalidData=@a3)?@a1=%27%2Fsites%2FAsutoshIntegration%2FShared%20Documents%2FGeneral%27&@a2=%27test7.txt%27&@a3=true&$Select=ServerRelativeUrl,UniqueId,Name,VroomItemID,VroomDriveID,ServerRedirectedUrl&$Expand=ListItemAllFields";
+        GET_URL = URL + "_api/web/GetFolderByServerRelativePath(DecodedUrl=@a1)/Files/AddUsingPath(DecodedUrl=@a2,AutoCheckoutOnInvalidData=@a3)?@a1=%27%2Fsites%2FAsutoshIntegration%2FShared%20Documents%2FGeneral%27&@a2=%27" + FilePath + "%27&@a3=true&$Select=ServerRelativeUrl,UniqueId,Name,VroomItemID,VroomDriveID,ServerRedirectedUrl&$Expand=ListItemAllFields";
         SecureStoreService secureStoreService = ITApiFactory.getService(SecureStoreService.class, null);
         UserCredential userCredential = secureStoreService.getUserCredential(Credential);
         char[] ch = userCredential.getPassword();
@@ -86,15 +98,15 @@ public class SharepointComponentProducer extends DefaultProducer {
         JsonNode jsonNode = mapper.readTree(result);
         String Token = jsonNode.get("access_token").asText();
         //Final Call
-        String finalResult = sendFiletoSharepoint(GET_URL,Token,input);
+        String finalResult = sendFiletoSharepoint(GET_URL,Token,is);
 
 		if(URL == null || URL.isEmpty()) {
 			URL = "(Producer) Hello!";
 		}
 		String messageInUpperCase = URL.toUpperCase();
 		if (input != null) {
-		    LOG.debug(input);
-			messageInUpperCase = input + " (Producer) : " + messageInUpperCase  + finalResult;
+
+			messageInUpperCase = type + finalResult;
 		}
 		exchange.getIn().setBody(messageInUpperCase);
         System.out.println(messageInUpperCase);
@@ -114,13 +126,14 @@ public class SharepointComponentProducer extends DefaultProducer {
         return result;
     }
 
-    private static String sendFiletoSharepoint(String url, String Token, String body) throws IOException {
+    private static String sendFiletoSharepoint(String url, String Token, InputStream body) throws IOException {
         String result = "";
         HttpPost post = new HttpPost(url);
         post.addHeader("Authorization","Bearer "+Token);
-        StringEntity b = new StringEntity(body);
-        post.setEntity(b);
-        try (CloseableHttpClient httpClient = HttpClients.createDefault();
+        byte[] bytes = IOUtils.toByteArray(body);
+        ByteArrayEntity entity = new ByteArrayEntity(bytes,ContentType.APPLICATION_OCTET_STREAM);
+        post.setEntity(entity);
+        try (CloseableHttpClient httpClient = HttpClients.custom().build();
              CloseableHttpResponse response = httpClient.execute(post)){
             result = EntityUtils.toString(response.getEntity());
         }
